bb5e5486e693a16ac65463298cc69776cfbcdf00
ivan <leo.wimc@gmail.com>
2022-12-29 20:37:44 -0500

    primer_ejercicio

---
+++ src/main/java/org/imoran/primerejercicio/Main.java
@@ -0,0 +1,12 @@
+package org.imoran.primerejercicio;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 10;
+        String nombre = "moran";
+        boolean x = true;
+        char c= 'x';
+        System.out.println("tipo de dato int " +numero +   "tipo de dato String  " + nombre + " boolean"+ x+ " char" + c);
+
+    }
+}
